bfdd7660817ade13890406190eda1c0e5b12bb32
Harshana Abeyaratne <harshana.abeyaratne@gmail.com>
2017-01-31 18:32:41 +0530

    Initial commit

---
+++ CalendarSample/Calendar.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    Dimensions,
+    Platform,
+    TouchableOpacity
+} from 'react-native';
+import moment from 'moment';
+
+const width = Dimensions.get('window').width;
+export class Calendar extends Component {
+    constructor() {
+        super()
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            ds: ds,
+            loadedDays: [],
+            firstVisibleDay: moment()
+        }
+
+        this.onSelectDate = this.onSelectDate.bind(this)
+    }
+
+    componentDidMount() {
+        this.setState({
+            ds: this.state.ds.cloneWithRows([...this.getInitialDates()]),
+        })
+
+        Platform.OS === 'ios' ? this._calendar.scrollTo({ x: width * 2, y: 0, animated: false }) : setTimeout(() => {
+            this._calendar.scrollTo({ x: width * 2, y: 0, animated: false })
+        }, 200)
+    }
+
+    getInitialDates() {
+        const firstDay = moment().subtract(Math.round(this.props.daysInRow * 2 + this.props.daysInRow / 2), 'd');
+        let days = [];
+        for (let i = 1; i < this.props.daysInRow * 5; i++) {
+            days.push({ day: moment(firstDay).add(i, 'days') });
+        }
+        this.setState({
+            loadedDays: [...days]
+        });
+        return days;
+    }
+
+    loadNextWeek(nextDay) {
+        let days = [];
+        for (let i = 1; i <= this.props.daysInRow; i++) {
+            days.push({ day: nextDay.day.clone().add(i, 'days') });
+        }
+        this.setState({
+            ds: this.state.ds.cloneWithRows([...this.state.loadedDays, ...days]),
+            loadedDays: [...this.state.loadedDays, ...days]
+        });
+    }
+
+    loadPreviousWeek(nextDay) {
+        let days = [];
+        for (let i = this.props.daysInRow; i >= 1; i--) {
+            days.push({ day: nextDay.day.clone().subtract(i, 'days') });
+        }
+
+        this.setState(() => {
+            return {
+                ds: this.state.ds.cloneWithRows([...days, ...this.state.loadedDays]),
+                loadedDays: [...days, ...this.state.loadedDays]
+            }
+        },
+            () => {
+                this._calendar.scrollTo({ x: width * 2, y: 0, animated: false })
+            });
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.props.renderCalendarHeader(this.state.firstVisibleDay)}
+                <ListView
+                    ref={calendar => this._calendar = calendar}
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}
+                    automaticallyAdjustContentInsets
+                    onMomentumScrollEnd={(event) => this.scrollEnded(event)}
+                    scrollEventThrottle={500}
+                    onChangeVisibleRows={(event) => { this.onChangeVisibleRows(event) } }
+                    onEndReached={() => { this.onEndReached() } }
+                    onEndReachedThreshold={width}
+                    dataSource={this.state.ds}
+                    renderRow={this.props.renderCalendarCell}
+                    />
+            </View>
+        );
+    }
+
+    scrollEnded(event) {
+        //console.log(event.nativeEvent.contentOffset.x);
+        if (event.nativeEvent.contentOffset.x <= width) {
+            this.loadPreviousWeek(this.state.loadedDays[0]);
+        }
+    }
+
+    onEndReached() {
+        const lastDayInList = this.state.loadedDays[this.state.loadedDays.length - 1];
+        this.loadNextWeek(lastDayInList);
+    }
+
+    onChangeVisibleRows(event) {
+        const firstVisibleRowId = Number(Object.keys(event.s1)[0]) + 1;
+        this.setState({
+            firstVisibleDay: this.state.loadedDays[firstVisibleRowId].day
+        })
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 110
+    }
+});
---
+++ CalendarSample/index.ios.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+import { Calendar } from './Calendar'
+
+const width = Dimensions.get('window').width;
+export default class CalendarSample extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      selectedDate: new Date().toISOString()
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Calendar
+          daysInRow={7}
+          renderCalendarHeader={(firstVisibleDay) => {
+            return (
+              <View style={{ height: 50, justifyContent: 'center', backgroundColor: '#1F508A', paddingLeft: 20 }}>
+                <Text style={{ color: '#fff', fontSize: 20 }}>{firstVisibleDay.format('MMMM YYYY')}</Text>
+              </View>
+            )
+          } }
+
+          renderCalendarCell={(rowData, sectionID, rowID) => {
+            return (
+              <View key={rowData.day.format("DDMMYYYY")} style={{ width: width / 7, height: 60, backgroundColor: '#fff', borderBottomWidth: 1, borderBottomColor: 'rgba(100,100,100,0.1)' }}>
+            <TouchableOpacity  
+            underlayColor='#008b8b' 
+            style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: rowData.day.isSame(new Date(), "day") ? 'rgba(141, 196, 61,.1)' : null , alignItems: 'center', paddingBottom: 10 }}
+                  onPress={()=>{ this.setState({selectedDate : rowData.day.format("YYYY-MM-DD") }) }}
+                  >
+                  <Text style={{ fontSize: 12, marginBottom: 2, color: 'rgba(0,0,0,0.6)', fontWeight: '400' }}>{rowData.day.format('ddd')}</Text>
+                  <Text style={{ fontSize: 16, color: 'rgba(0,0,0,0.9)', fontWeight: '400' }}>{rowData.day.format('D')}</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          } }
+          
+          />
+
+  <Text style={{marginTop: 100, fontSize: 18}}>Selected Date : {this.state.selectedDate.substring(0,10)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 100,
+    backgroundColor: '#f8f8f8'
+  }
+});
+
+AppRegistry.registerComponent('CalendarSample', () => CalendarSample);
